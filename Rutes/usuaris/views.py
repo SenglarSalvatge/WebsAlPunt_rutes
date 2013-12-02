3fb1ad3a41eb6949ab52069aebcab470a230e348
Rodrigo Saulis Mendez <rodrigosaulis@gmail.com>
2013-12-02 19:18:13 +0100

    Vistas reparadas

--- Rutes/usuaris/views.py
+++ Rutes/usuaris/views.py
@@ -1,4 +1,5 @@
+#-*- coding: utf-8 -*-
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Benvingut a la pàgina principal de Activi.cat.")
+    return HttpResponse(u"Benvingut a la pàgina principal de Activi.cat.")
