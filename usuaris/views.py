ee910c8ec980f2753378c7530f429e9db54ecff9
darwin <darwin@darwin.(none)>
2013-12-20 16:01:23 +0100

    Acabat auth

--- usuaris/views.py
+++ usuaris/views.py
@@ -1,5 +1,10 @@
 #-*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse(u"Benvingut a la pàgina principal de Activi.cat.")
+    return HttpResponse(u"Benvingut a la pàgina principal de Activi.cat")
+
+def login(request):
+    #todo: fer render de login_social.html
+    return render(request, 'login_social.html', {})
