ab76d030040eda6322950dbdbb5acdfd3edf2211
SenglarPeris <esqueesmoltllarga@inolaescric.mai>
2013-12-20 16:14:46 +0100

    0

--- usuaris/urls.py
+++ usuaris/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from usuaris import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.mostrarPerfil, name='mostrarPerfil')
+    url(r'^/$', views.mostrarPerfil, name='mostrarPerfil')
 )
