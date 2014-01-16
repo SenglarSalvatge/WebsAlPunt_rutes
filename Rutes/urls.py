a377c425553c9f9c5c11c9e1abea7228dfa589fa
Rodrigo Saulis Mendez <rodrigosaulis@gmail.com>
2014-01-16 15:48:37 +0100

    Alex lo peta!!!!

--- Rutes/urls.py
+++ Rutes/urls.py
@@ -11,13 +11,13 @@ urlpatterns = patterns('',
     url(r'^$', 'Rutes.views.index', name='index'),
     
     #mostrem l'apartat de rutes: /rutes/
-    url(r'^rutes/$', include('posts.urls', namespace='posts')),
+    url(r'^rutes/', include('posts.urls', namespace='posts')),
     
     #mostrem el perfil de l'usuari: /usuaris/
-    url(r'^usuaris/$', include('usuaris.urls', namespace='usuaris')),
+    url(r'^usuaris/', include('usuaris.urls', namespace='usuaris')),
     
     #directori d'administració: /admin/
-    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     url('', include('social.apps.django_app.urls', namespace='social'))
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
