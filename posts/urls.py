59a1667fc04db8322e06b5d8c69a0e51f38486f5
SenglarPeris <esqueesmoltllarga@inolaescric.mai>
2014-01-17 15:38:52 +0100

    Martí:
    	JocdeProves
    	Vistes

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from posts import views
 
 urlpatterns = patterns('',
-    url(r'^rutes/$', views.mostrarRutes, name='mostrarRutes'),
+    url(r'ruta$', views.mostrarRutes, name='mostrarRutes'),
     
     #proves per a google maps
-    url(r'^mapes/$', views.google, name='google'),
+    url(r'mapes/$', views.google, name='google'),
     #url(r'^mapes/resultats/$', views.googleResultats, name='googleResultats'),
 )
--- usuaris/urls.py
+++ usuaris/urls.py
@@ -5,10 +5,10 @@ from usuaris import views
 urlpatterns = patterns('',
     
     #mostrem el perfil d'usuari: /usuaris/perfil
-    url(r'^usuaris/perfil/$', views.mostrarPerfil, name='mostrarPerfil'),
+    url(r'perfil/$', views.mostrarPerfil, name='mostrarPerfil'),
     
     #mostrem l'autenticacio per social auth: /usuaris/autenticacio
-    url(r'^usuaris/autenticacio/$', views.login, name='login')
+    url(r'autenticacio/$', views.login, name='login')
 
 )
 
