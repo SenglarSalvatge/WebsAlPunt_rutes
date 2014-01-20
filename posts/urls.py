27201eee5ec398f8e5a3b4a1eda4d7aa26dd102a
SenglarPeris <esqueesmoltllarga@inolaescric.mai>
2014-01-20 19:33:42 +0100

    qwert

--- posts/urls.py
+++ posts/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, url
 from posts import views
 
 urlpatterns = patterns('',
-    url(r'ruta$', views.mostrarRutes, name='mostrarRutes'),
+    url(r'^ruta/$', views.mostrarRutes, name='mostrarRutes'),
     
     #proves per a google maps: CLIENT
-    url(r'mapes/$', views.google, name='google'),
+    url(r'^mapes/$', views.google, name='google'),
     
     #proves per a google maps: CLIENT
     url(r'^mapes/resultats/$', views.googleResultats, name='googleResultats'),
