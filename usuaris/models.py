ce08f6eb9fca6ef0eb4b680bb9dabb0f32f254cc
Rodrigo Saulis Mendez <rodrigosaulis@gmail.com>
2014-01-24 15:40:33 +0100

    Miedo me da

--- usuaris/models.py
+++ usuaris/models.py
@@ -1,18 +1,20 @@
 # -*- encoding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
+from usuaris.nouNick import crearNick
 
 class Perfil(models.Model):
     #foto //encara en fase de desenvolupament
     descripcio = models.CharField(max_length=800, help_text="Descripció", blank=True)
     puntuacio = models.IntegerField(help_text="Puntuació", default = 0)
-    #nick = 
-    usuari = models.OneToOneField(User, help_text="Usuari")
+    nick = models.CharField(max_length=50, help_text="El teu Nickname", blank=False) 
+    usuari = models.OneToOneField(User, help_text="Usuari")    
     
     def __unicode__(self):  
-        return self.usuari.username
+        return self.nick
 
 
+#Antes de crear usuarios chupis
 from django.db.models.signals import post_save
 
 # definition of UserProfile from above
@@ -20,8 +22,8 @@ from django.db.models.signals import post_save
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        #generar nick
-        Perfil.objects.create(usuari=instance, descripcio="" )
+        nick = crearNick()
+        Perfil.objects.create(usuari=instance, descripcio="", nick = nick )
 
-#connectem event: després de que es crea l'usuari s'invoca la funció 'create_user_profile'
+# connectem event: després de que es crea l'usuari s'invoca la funció 'create_user_profile'
 post_save.connect(create_user_profile, sender=User)
--- usuaris/views.py
+++ usuaris/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.contrib.auth import logout
+from models import Perfil
+from django.shortcuts import get_object_or_404
 
 def mostrarPerfil(request):
     return render(request, 'mostrarPerfil.html')
@@ -12,3 +14,14 @@ def login(request):
 def exit_login(request):
     logout(request)
     return render(request, 'index.html', {})
+
+def loginUsuari(request):
+    variable = request.GET.get('access_token')
+    if variable is not None:
+        p = Perfil.objects.filter(token=variable)
+    else:
+        p = Perfil()
+        p.nick = "AleX"
+        p.token = variable
+        p.save()
+    return render(request, 'usuaris/mostrarRequest.html')
